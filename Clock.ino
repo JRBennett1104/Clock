b976fd1f4285111702509cf867c109840c23d1bc
Jack Bennett <jack.bennett.au@gmail.com>
2018-08-30 21:31:54 +1000

    Added RTC initialisation
    Added code to initialise the RTC after it's lost power or is otherwise uninitialised.

--- Clock.ino
+++ Clock.ino
@@ -1,3 +1,4 @@
+#include <Wire.h>
 #include <RTClib.h>
 
 const int DISCO = 0;
@@ -11,10 +12,14 @@ int counter = 0;
 int mode = true;
 int countdown = 1234;
 
+RTC_DS3231 rtc;
+
 void setup() {
-  for(int pin = 0; pin < 14; pin++ {
+  for(int pin = 0; pin < 14; pin++) {
     pinMode(pin, OUTPUT);
   }
+
+  initialiseRtc();
 }
 
 void loop() {
@@ -98,3 +103,26 @@ void setDigitValue(int digit, int value) {
   digitalWrite(value, LOW);
 }
 
+void initialiseRtc() {
+  #ifndef ESP8266
+    while (!Serial); // for Leonardo/Micro/Zero
+  #endif  
+
+  Serial.begin(9600);
+
+  delay(3000); // wait for console opening
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, lets set the time!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+}
