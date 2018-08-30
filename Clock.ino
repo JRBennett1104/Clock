5ae63cbb732b49a0b099739a595d216c60909254
Jack Bennett <jack.bennett.au@gmail.com>
2018-08-30 21:26:00 +1000

    Change pin init to loop
    Switch individual pin initialisation to a loop.

--- Clock.ino
+++ Clock.ino
@@ -12,20 +12,9 @@ int mode = true;
 int countdown = 1234;
 
 void setup() {
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
+  for(int pin = 0; pin < 14; pin++ {
+    pinMode(pin, OUTPUT);
+  }
 }
 
 void loop() {
